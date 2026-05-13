4c063c7e1ea547479089cf8fed89b14901157760
Skywalkr-dev <snaveenbharath2005@gmail.com>
2026-05-13 13:04:02 +0530

    feat: added local event logs

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
 )
+
+func watchEvent(watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Has(fsnotify.Write) {
+				fmt.Println("Modified file : ", event.Name)
+			} else if event.Has(fsnotify.Create) {
+				fmt.Println("Created file : ", event.Name)
+				watcher.Add(event.Name)
+			} else if event.Has(fsnotify.Remove) {
+				fmt.Println("Removed file : ", event.Name)
+				watcher.Remove(event.Name)
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			fmt.Println(err)
+		}
+	}
+}
 
 func main() {
 	watcher, _ := fsnotify.NewWatcher()
@@ -23,5 +49,6 @@ func main() {
 		return nil
 	}
 	filepath.Walk("./test-dir", watchDir)
-
+	go watchEvent(watcher)
+	select {}
 }
